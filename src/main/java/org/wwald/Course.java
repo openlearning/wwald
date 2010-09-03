d784def2140d766767f1c61b6ddf1a1716655fce
Parag Shah <adamin@adaptivelearningonline.net>
2010-09-03 10:56:30 +0530

    list of courses being generated by canned classes instead of canned html

---
+++ src/main/java/org/wwald/Course.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.wwald;
+
+/**
+ * @author pshah
+ *
+ */
+public class Course {
+	private String title;
+	private String description;
+	
+	public Course(String title, String description) {
+		this.title = title;
+		this.description = description;
+	}
+	
+	public Course() {
+		
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
--- src/main/java/org/wwald/HomePage1.java
+++ src/main/java/org/wwald/HomePage1.java
@@ -1,7 +1,13 @@
 package org.wwald;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.WebPage;
 
 /**
@@ -21,16 +27,38 @@ public class HomePage1 extends WebPage {
 	 */
     public HomePage1(final PageParameters parameters) {
 
-        // Add the simplest type of label
-    	add(new Label("course1.title", "Understanding Computers And The Internet"));
-    	add(new Label("course1.description", "This course is all about understanding: understanding what's going on inside your computer when you flip on the switch, why tech support has you constantly rebooting your computer, how everything you do on the Internet can be watched by ..."));
-    	
-    	add(new Label("course2.title", "Introduction To Computer Science"));
-    	add(new Label("course2.description", "Introduction to Computer Science I is a first course in computer science at Harvard College for concentrators and non-concentrators alike. More than just teach you how to program, this course teaches you how to think more methodically and how to ..."));
-    	
-    	add(new Label("course3.title", "Introduction to Computer Science and Programming (using Python)"));
-    	add(new Label("course3.description", "This subject is aimed at students with little or no programming experience. It aims to provide students with an understanding of the role computation can play in solving problems. It also aims to help students, regardless of their major, to ..."));
+    	add(new ListView("courses", getCourses()) {
 
-        // TODO Add your page's components here
+			@Override
+			protected void populateItem(ListItem item) {
+				Course course = (Course)item.getModelObject();
+				Link courseLink = new Link("goto.course", item.getModel()) {
+					@Override
+					public void onClick() {
+						// TODO Auto-generated method stub
+						
+					}
+				};
+				courseLink.add(new Label("course.title", course.getTitle()));
+				item.add(courseLink);
+				item.add(new Label("course.description", course.getDescription()));
+			}
+    		
+    	});
+    }
+    
+    private List<Course> getCourses() {
+    	List<Course> courses = new ArrayList<Course>();
+    	
+    	courses.add(new Course("Understanding Computers And The Internet", 
+    						   "This course is all about understanding: understanding what's going on inside your computer when you flip on the switch, why tech support has you constantly rebooting your computer, how everything you do on the Internet can be watched by ..."));
+
+    	courses.add(new Course("Introduction To Computer Science", 
+    						   "Introduction to Computer Science I is a first course in computer science at Harvard College for concentrators and non-concentrators alike. More than just teach you how to program, this course teaches you how to think more methodically and how to ..."));
+
+    	courses.add(new Course("Introduction to Computer Science and Programming (using Python)", 
+    						   "This subject is aimed at students with little or no programming experience. It aims to provide students with an understanding of the role computation can play in solving problems. It also aims to help students, regardless of their major, to ..."));
+    	
+    	return courses;
     }
 }
