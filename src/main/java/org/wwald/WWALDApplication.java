f16d1838ad18353cb9940bdd6ee468de9b01ce8c
Parag Shah <adamin@adaptivelearningonline.net>
2010-10-08 12:08:54 +0530

    added Registration view pages

--- src/main/java/org/wwald/WWALDApplication.java
+++ src/main/java/org/wwald/WWALDApplication.java
@@ -8,6 +8,7 @@ import org.wwald.view.CoursePage;
 import org.wwald.view.ErrorPage404;
 import org.wwald.view.HomePage;
 import org.wwald.view.Login;
+import org.wwald.view.Register;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
@@ -40,6 +41,7 @@ public class WWALDApplication extends WebApplication
 //		mount(new IndexedParamUrlCodingStrategy("courses", CoursePage.class));
 		mountBookmarkablePage("courses", CoursePage.class);
 		mountBookmarkablePage("login", Login.class);
+		mountBookmarkablePage("register", Register.class);
 		mount(new QueryStringUrlCodingStrategy("error404", ErrorPage404.class));
 	}
 	
