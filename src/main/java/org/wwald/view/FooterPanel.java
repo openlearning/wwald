761b839f3d09db63b9b667d8fcbf81fad974d457
Parag Shah <adamin@adaptivelearningonline.net>
2010-10-30 14:38:39 +0530

    using wicket links in the footer

--- src/main/java/org/wwald/view/FooterPanel.java
+++ src/main/java/org/wwald/view/FooterPanel.java
@@ -1,9 +1,24 @@
 package org.wwald.view;
 
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.wwald.WicketIdConstants;
 
 public class FooterPanel extends Panel {
 	public FooterPanel(String id) {
 		super(id);
+		
+		Link tosLink = 
+			new BookmarkablePageLink(WicketIdConstants.TOS_LINK, HomePage.class);
+		add(tosLink);
+		
+		Link privacyPolicyLink = 
+			new BookmarkablePageLink(WicketIdConstants.PRIVACY_POLICY_LINK, HomePage.class);
+		add(privacyPolicyLink);
+		
+		Link contactLink = 
+			new BookmarkablePageLink(WicketIdConstants.CONTACT_LINK, HomePage.class); 
+		add(contactLink);
 	}
 }
