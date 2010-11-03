eb6739b2c1cebdd45d23df6be7ded67a380974fc
Parag Shah <adamin@adaptivelearningonline.net>
2010-11-03 14:56:59 +0530

    added an About page

---
+++ src/main/java/org/wwald/view/AboutPage.java
@@ -0,0 +1,11 @@
+package org.wwald.view;
+
+import org.apache.wicket.PageParameters;
+
+public class AboutPage extends BasePage {
+
+	public AboutPage(PageParameters parameters) {
+		super(parameters);
+	}
+
+}
