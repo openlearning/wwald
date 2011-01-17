ebf87d243b0e04ef98db9299339e21e048d609a1
Parag Shah <adamin@adaptivelearningonline.net>
2011-01-17 21:33:15 +0530

    Added some missing tests to AllTests

--- src/test/java/org/wwald/AllTests.java
+++ src/test/java/org/wwald/AllTests.java
@@ -6,14 +6,18 @@ import org.wwald.model.DataFacadeRDBMSImplTest;
 import org.wwald.service.ApplicationFacadeTest;
 import org.wwald.util.TestCourseWikiParser;
 
+import util.TestObjectsRepository;
+
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+	TestHomePage.class,
 	TestPropertiesLoading.class,
 	//UsersFileParserTest.class,
 	//CourseFileParserTest.class,
 	DataFacadeRDBMSImplTest.class,
 	ApplicationFacadeTest.class,
 	TestCourseWikiParser.class,
+	TestObjectsRepository.class,
 })
 public class AllTests {
 
